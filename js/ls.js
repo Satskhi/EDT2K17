78083c8791c8b1ec27a28263f8239b57276f0061
Satskhi <w.wesley.af@gmail.com>
2017-02-14 14:03:40 -0200

    Update ls.js

--- js/ls.js
+++ js/ls.js
@@ -2,7 +2,7 @@ API.on(API.ADVANCE, function () {
 var user = API.getDJ().username;
     let media = API.getMedia().cid;
 	const nsfw = ["tf52mbGTB4o", "Rg6yj4f1KWA", "xVwb9Ws10Kc", "tMH3EqrOz2U", "9W8Q9FvH1XA", "O_QxG1rPW8Q", "wsUQKw4ByVg", "0W6HxYGth20", "Ei4rbJgMZPc"];
-	const op = ["NSTx31dxmgA", "zM2kobGhWvo", "kcuVvryU9QA", "5hEh9LiSzow", "qn-X5A0gbMA", "Rpr_HNJ0Y3A", "GVmw6wnL4oQ"];
+	const op = ["f9EM8T5K6d8", "NSTx31dxmgA", "zM2kobGhWvo", "kcuVvryU9QA", "5hEh9LiSzow", "qn-X5A0gbMA", "Rpr_HNJ0Y3A", "GVmw6wnL4oQ"];
 	const theme = ["E5ONTXHS2mM"];
 	const fkthissong = ["291805417", "8QJV1sOm040"];
 
