fa0cc9298465e787759ad1a36afc06dc4b76f3d2
Satskhi <w.wesley.af@gmail.com>
2017-03-05 20:25:55 -0300

    Update lsV2.js

--- js/lsV2.js
+++ js/lsV2.js
@@ -19,28 +19,19 @@ var user = API.getDJ().username;
     else if (op.indexOf(media) > -1) {
 		API.sendChat("/me @"+user+" Song is OP");
         setTimeout(function() {
-            API.sendChat("!skip");
-		setTimeout(function() {
-			API.sendChat("/move @"+user+" 2");
-		}, 1300);
+            API.sendChat("!lockskip");
         }, 1300);
     }
 	else if (theme.indexOf(media) > -1) {
 		API.sendChat("/me @"+user+" Song is off theme");
         setTimeout(function() {
-            API.sendChat("!skip");
-		setTimeout(function() {
-			API.sendChat("/move @"+user+" 2");
-		}, 1300);
+            API.sendChat("!lockskip");
         }, 1300);
     }
 	else if (fkthissong.indexOf(media) > -1) {
 		API.sendChat("/me @"+user+" Song without quality :facepalm:");
         setTimeout(function() {
-            API.sendChat("!!skip");
-		setTimeout(function() {
-			API.sendChat("/move @"+user+" 2");
-		}, 1300);
+            API.sendChat("!lockskip");
         }, 1300);
     }
 	else {
