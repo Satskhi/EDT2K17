28559f10b8266a4ce2cc292d7fe71a65b97d4958
Satskhi <w.wesley.af@gmail.com>
2017-03-06 10:40:10 -0300

    Update lsV2.js

--- js/lsV2.js
+++ js/lsV2.js
@@ -5,7 +5,7 @@ API.off(API.ADVANCE);
 API.on(API.ADVANCE, function () {
 var user = API.getDJ().username;
     let media = API.getMedia().cid;
-	const nsfw = ["tf52mbGTB4o", "Rg6yj4f1KWA", "xVwb9Ws10Kc", "tMH3EqrOz2U", "9W8Q9FvH1XA", "O_QxG1rPW8Q", "wsUQKw4ByVg", "0W6HxYGth20", "Ei4rbJgMZPc"];
+	const nsfw = ["PMCVHudpjgE","tf52mbGTB4o", "Rg6yj4f1KWA", "xVwb9Ws10Kc", "tMH3EqrOz2U", "9W8Q9FvH1XA", "O_QxG1rPW8Q", "wsUQKw4ByVg", "0W6HxYGth20", "Ei4rbJgMZPc"];
 	const op = ["1sXyBpbpHSk", "vZv9-TWdBJM", "Iw-6Y-jz9Kc", "f9EM8T5K6d8", "NSTx31dxmgA", "zM2kobGhWvo", "kcuVvryU9QA", "5hEh9LiSzow", "qn-X5A0gbMA", "Rpr_HNJ0Y3A", "GVmw6wnL4oQ"];
 	const theme = ["E5ONTXHS2mM"];
 	const fkthissong = ["291805417", "8QJV1sOm040"];
